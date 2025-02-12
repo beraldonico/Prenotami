9f7292d2ca4a54202f87b85ef3b220e9719f6930
Nicolas Beraldo <nicolas.beraldo@outlook.com>
2025-02-12 16:33:30 -0300

    disable not available msg

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
 		if Service_avaiable:
 			bot.send_message(f"Agendamento pode estar diponivel, acessar {os.getenv('URL_SERVICO')} para tentar realizar o agendamento")
 		else:
-			bot.send_message(f"Agendamento ainda indisponivel!")
-			#pass
-		
+			#bot.send_message(f"Agendamento ainda indisponivel!")
+			pass
+
 		MainDriver.Dispose()
 		print(f"{datetime.now().strftime(f"%Y/%m/%d %H:%M:%S")} - Process finish with sucess status! The avaiability of the service is {Service_avaiable}")
 
